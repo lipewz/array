3690d6415d98f9d0b1fa1fdad0fa6a2c5683cf4b
lipewz <felipecosta.augusto2008@gmail.com>
2025-03-31 08:59:14 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+var nomes = ["Elias", "Gabriel", "Erick", "João", "Arthur", "Clara", "Clara"]
+var nomeEscolhido = parseInt(prompt("Digite um número de 0 a 14"))
+document.write("Nome escolhido" + nomes[nomeEscolhido])
